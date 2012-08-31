b08b470f2a0af8b17ae95ac00e9acc1358fc3e39
descent <descent@weiya.com.tw>
2012-08-31 16:18:50 +0800

    test Queue push/pop operator

--- main.cpp
+++ main.cpp
@@ -1,16 +1,85 @@
 #include "queue.h"
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+  Queue q(5);
+void push_test(char c)
+{
+    cout << "  begin: " << q.begin() << endl;
+    cout << "  end: " << q.end() << endl;
+    if (q.push(c))
+      cout << "push: " << c << endl;
+    else
+      cout << "cannot push: " << c << endl;
+    cout << "    begin: " << q.begin() << endl;
+    cout << "    end: " << q.end() << endl;
+    q.print();
+    cout << "==============================" << endl;
+}
+
+void pop_test()
+{
+  char c;
+    cout << "  begin: " << q.begin() << endl;
+    cout << "  end: " << q.end() << endl;
+
+    if (q.pop(c))
+      cout << "pop: " << c << endl;
+    else
+      cout << "pop fail" << endl;
+
+    cout << "    begin: " << q.begin() << endl;
+    cout << "    end: " << q.end() << endl;
+    q.print();
+    cout << "==============================" << endl;
+}
 
 // how to autotest for class Queue
 int main(void)
 {
-  Queue q(5);
-  q.push('a');
-  q.push('b');
-  q.push('c');
-  q.push('d');
-  q.push('e');
-  q.push('f');
-  q.push('g');
-  q.print();
+  char test_pattern[]=
+  {
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+  };
+  srand(time(NULL));
+
+  int times = 0;
+  while(1)
+  {
+    int num = rand() % (sizeof(test_pattern)/sizeof(char));
+    cout << "  num: " << num << endl;
+
+    if (num % 2 == 0)
+      push_test(test_pattern[num]);
+    else
+      pop_test();
+
+    ++times;
+    if (times >= 100)
+      break;
+  }
+
   return 0;
 }
