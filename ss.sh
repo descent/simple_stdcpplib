3a86a09ab80c31cb117889e9bd57cad4788c8a1a
descent <descent0@gmail.com>
2014-10-29 20:18:02 +0800

    new file:   ss.sh

---
+++ ss.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+ln -s ~/git/simple_scheme/token_container.h .
+ln -s ~/git/simple_scheme/token_container.cpp .
+ln -s ~/git/simple_scheme/s_eval.h .
+ln -s ~/git/simple_scheme/s_eval.cpp  .
+ln -s ~/git/simple_scheme/cell.h .
+ln -s ~/git/simple_scheme/cell.cpp .
+
