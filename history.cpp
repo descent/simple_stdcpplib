6ebdcff770dfa9f66a36198083347baef82f286a
descent <descent0@gmail.com>
2014-11-04 19:15:12 +0800

    new file:   history.cpp

---
+++ history.cpp
@@ -0,0 +1,67 @@
+#include <ncurses.h>
+#include <locale.h>
+#include <string>
+
+using namespace std;
+
+#include "gdeque.h"
+
+int main()
+{
+  setlocale(LC_ALL, "");
+
+  initscr();
+  keypad(stdscr,true);
+  curs_set(0);
+  // KEY_UP
+  // KEY_DOWN
+
+  Deque<string> deque;
+
+  deque.init();
+
+  move(0,0);
+  while(1)
+  {
+    char str[128];
+    getstr(str);
+    move(0,0);
+    std::string s(str);
+    deque.push_back(s);
+
+    int index=0;
+    noecho();
+    while(1)
+    {
+      int ch = getch();
+      switch(ch)
+      {
+        case KEY_UP:
+        {
+          string ps;
+          if (deque.back(index, ps) == true)
+          {
+            ++index;
+            mvprintw(0, 15, "%s", ps.c_str());
+          }
+          break;
+        }
+        case KEY_DOWN:
+        {
+          break;
+        }
+        default:
+        {
+          ungetch(ch);
+          goto outer;
+          break;
+        }
+      }
+    }
+    outer:
+    echo();
+  }
+
+  endwin();
+  return 0;
+}
