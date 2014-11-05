eec006cff6d65604573d0e03f895b67434ca16eb
descent <descent0@gmail.com>
2014-11-05 10:46:08 +0800

    use CString to test

--- history.cpp
+++ history.cpp
@@ -3,6 +3,7 @@
 #include <string>
 
 #include "gdeque.h"
+#include "cstring.h"
 
 using namespace std;
 using namespace DS;
@@ -10,6 +11,32 @@ using namespace DS;
 
 int main()
 {
+
+#if 0
+  CString cstr;
+
+  cstr.init("abc");
+  cstr.print();
+  printf("\n");
+
+  Deque<CString> deque;
+  deque.init();
+
+  deque.push_back(cstr);
+
+  CString ps;
+  ps = "102";
+
+  cstr.init("def");
+  deque.push_back(cstr);
+
+  deque.print();
+  deque.back(0, ps);
+  ps.print();
+  printf("\n");
+#endif
+
+#if 1
   setlocale(LC_ALL, "");
 
   initscr();
@@ -18,7 +45,8 @@ int main()
   // KEY_UP
   // KEY_DOWN
 
-  Deque<string> deque;
+  //Deque<string> deque;
+  Deque<CString> deque;
 
   deque.init();
 
@@ -29,7 +57,9 @@ int main()
     char str[128];
     getstr(str);
     move(0,0);
-    std::string s(str);
+    //std::string s(str);
+    CString s;
+    s.init(str);
     if (deque.push_back(s) == false)
     {
       mvprintw(20, 0, "push back fail ## %d", fail_time++);
@@ -41,7 +71,8 @@ int main()
 
     int index=0;
     noecho();
-    string ps;
+    //string ps;
+    CString ps;
     while(1)
     {
       mvprintw(17, 0, "index: %d", index);
@@ -94,5 +125,6 @@ int main()
   }
 end:
   endwin();
+#endif
   return 0;
 }
