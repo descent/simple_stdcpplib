5cb9b716006614d216a67e16616d40f14cc6a45b
descent <descent0@gmail.com>
2014-11-17 10:25:55 +0800

    add needed symbolic links

--- ss.sh
+++ ss.sh
@@ -6,3 +6,9 @@ ln -s ~/git/simple_scheme/s_eval.cpp  .
 ln -s ~/git/simple_scheme/cell.h .
 ln -s ~/git/simple_scheme/cell.cpp .
 
+ln -s /home/descent/git/progs/queue/gdeque.h .
+ln -s /home/descent/git/progs/queue/gdeque.cpp .
+
+ln -s /home/descent/git/progs/queue/cstring.h .
+ln -s /home/descent/git/progs/queue/cstring.cpp .
+
