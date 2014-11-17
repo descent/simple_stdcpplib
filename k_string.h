7c99440aa73e25973153d142e4c78173b3cb03da
descent <descent0@gmail.com>
2014-11-17 10:21:16 +0800

    copy from:     stm32_p103_demos/demos/uart_echo

--- k_string.h
+++ k_string.h
@@ -29,6 +29,21 @@ static inline char* s_strcpy(char *dest, const char *src)
   return p;
 }
 
+static inline char* s_strncpy(char *dest, const char *src, int n)
+{
+  char *p = dest;
+  int i=0;
+  while(*src)
+  {
+    *dest++ = *src++;
+    ++i;
+    if (i >= n)
+      break;
+  }
+  *dest='\0';
+  return p;
+}
+
 static inline int s_strcmp(const char *s1, const char *s2)
 {
   s8 res;
@@ -197,13 +212,23 @@ static inline int s32_sprintf(char *str_buf, const char *fmt, ...)
   return p-str_buf - 1;
 }
 
-static inline int atoi(char *s)
+static inline int atoi(const char *s)
 {
   // ref: http://blog.csdn.net/wxhhkhl/article/details/6142289
   int n=0,i;
-  for(i=0;s[i]>='0'&&s[i]<='9';i++)
-  n=n*10+s[i]-'0';//将字符串转换为整型数字
-  return n;
+
+  if (s[0] == '-')
+    i = 1;
+  else
+    i = 0;
+
+  for(; s[i]>='0' && s[i]<='9';i++)
+    n=n*10+s[i]-'0';
+
+  if (s[0] == '-')
+    return -n;
+  else
+    return n;
 }
 
 static inline bool isdigit(int c)
@@ -212,6 +237,79 @@ static inline bool isdigit(int c)
     return true;
   else
     return false;
+}
+
+static inline int mypow(int x, int y)
+{
+  int r=1;
+  for (int i=0 ; i < y ; ++i)
+    r*=x;
+  return r;
+}
+
+const int PRECISION = 6;
+
+static inline u8* float_to_str(float num)
+{
+   int whole_part = num;
+   int digit = 0, reminder =0;
+   //int log_value = log10(num), 
+   int index;
+   float wt = 0.0;
+
+   // String containg result
+   static u8 str[30];
+
+   //Initilise stirng to zero
+   //memset(str, 0 ,20);
+   
+   //u8 whole_str[20] ;
+   s32_itoa(whole_part, str, 10);
+#if 0
+   //Extract the whole part from float num
+   for(int  i = 1 ; i < log_value + 2 ; i++)
+   {
+       wt  =  mypow(10,i);
+       reminder = whole_part  %  wt;
+       digit = (reminder - digit) / (wt/10);
+
+       //Store digit in string
+       str[index--] = digit + 48;              // ASCII value of digit  = digit + 48
+       if (index == -1)
+          break;    
+   }
+#endif
+    //index = log_value + 1;
+    index = s_strlen((const char*)str);
+    str[index] = '.';
+
+   float fraction_part  = num - whole_part;
+   float tmp1 = fraction_part,  tmp =0;
+
+#if 0
+   cout << "fraction_part: " << fraction_part << endl;
+   cout << "num: " << num << endl;
+   cout << "whole_part: " << whole_part << endl;
+#endif
+   //Extract the fraction part from  num
+   for( int i= 1; i < PRECISION; i++)
+   {
+      wt =10; 
+      tmp  = tmp1 * wt;
+      digit = tmp;
+      //cout << "digit: " << digit << endl;
+      //cout << "tmp: " << tmp << endl;
+      //cout << "tmp1: " << tmp1 << endl;
+
+      //Store digit in string
+      str[++index] = digit +48;           // ASCII value of digit  = digit + 48
+      tmp1 = tmp - digit;
+      if (tmp1 == 0.0)
+        break;
+   }    
+   str[index+1] = 0;
+
+   return str;
 }
 
 
