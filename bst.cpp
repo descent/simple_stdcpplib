4c6d5e17ed4fa554e32a0c2d2dd7288f3c518dae
descent <descent0@gmail.com>
2015-11-28 13:24:33 +0800

    add del(), free_node()

--- bst.cpp
+++ bst.cpp
@@ -38,6 +38,79 @@ NodeType *make_node(const Key &k, Value v)
   return n;
 }
 
+template <typename NodeType>
+void free_node(NodeType *n)
+{
+  delete n;
+}
+
+template <typename NodeType, typename Key>
+NodeType* del(NodeType *n, const Key &k)
+{
+  NodeType *p;
+
+  if (n==0)
+    return 0;
+  if (n->k_ > k)
+    n->l_ = del(n->l_, k);
+  else if (n->k_ < k)
+         n->r_ = del(n->r_, k);
+       else
+       {
+         if (n->l_ == 0 && n->r_ == 0)
+         {
+           free_node(n);
+           n = 0;
+         }
+         else if (n->l_) // it has left subtree
+              {
+	        for (p = n->l_; p->r_; p = p->r_);
+
+	        n->k_ = p->k_;
+	        n->v_ = p->v_;
+	        n->l_ = del(n->l_, n->k_);
+              }
+              else // it has right subtree
+              {
+	        for (p = n->r_; p->l_; p = p->l_);
+
+	        n->k_ = p->k_;
+	        n->v_ = p->v_;
+	        n->r_ = del(n->r_, n->k_);
+              }
+       }
+  return n;
+}
+
+#if 0
+{
+	link p;
+	if (!t)
+		return NULL;
+	if (t->item > key) /* delete from left subtree */
+		t->l = delete(t->l, key);
+	else if (t->item < key) /* delete from right subtree */
+		t->r = delete(t->r, key);
+	else { /* if (t->item == key) */
+		if (t->l == NULL && t->r == NULL) { /* if t is leaf node */
+			free_node(t);
+			t = NULL;
+		} else if (t->l) { /* if t has left subtree */
+			/* replace t with the rightmost node in left subtree */
+			for (p = t->l; p->r; p = p->r);
+			t->item = p->item;
+			t->l = delete(t->l, t->item);
+		} else { /* if t has right subtree */
+			/* replace t with the leftmost node in right subtree */
+			for (p = t->r; p->l; p = p->l);
+			t->item = p->item;
+			t->r = delete(t->r, t->item);
+		}
+	}
+	return t;
+}
+#endif
+
 template <typename NodeType, typename Key, typename Value>
 NodeType *insert(NodeType *t, const Key &k, Value v)
 {
@@ -147,6 +220,7 @@ NodeType* invert_tree(NodeType* root)
 
 int main(int argc, char *argv[])
 {
+#if 0
   GNode<std::string, int> *root = 0;
   root = insert(root, "abc", 3);
   root = insert(root, "def", 1);
@@ -163,16 +237,25 @@ int main(int argc, char *argv[])
   else
     cout << "found (" << n->k_ << ", " <<  n->v_<< ")" << endl;
 
+#else
   GNode<double, int> *r1 = 0;
+  r1 = insert(r1, 8.2, 7);
+  r1 = insert(r1, 4.2, 7);
   r1 = insert(r1, 1.2, 3);
   r1 = insert(r1, 3.2, 5);
   r1 = insert(r1, 2.2, 4);
-  r1 = insert(r1, 5.2, 7);
+  r1 = insert(r1, 3.4, 4);
   r1 = insert(r1, 5.8, 0);
+  r1 = insert(r1, 14.2, 7);
+  printf("\\tree");
+  print_tree(r1);
+  printf("\n");
+  del(r1, 3.2);
   printf("\\tree");
   print_tree(r1);
   printf("\n");
 
+#if 0
   GNode<double, int> *r2 = 0;
 
   r2 = invert_tree(r1);
@@ -181,6 +264,8 @@ int main(int argc, char *argv[])
   printf("\\tree");
   print_tree(r2);
   printf("\n");
+#endif
 
+#endif
   return 0;
 }
