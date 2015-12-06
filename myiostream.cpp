c72fdb92caffe5ab1e24072c41e35bc6ae19bacc
descent <descent0@gmail.com>
2015-12-06 23:16:50 +0800

    new file:   myiostream.cpp 	new file:   myiostream.h

---
+++ myiostream.cpp
@@ -0,0 +1,26 @@
+#include "myiostream.h"
+
+#include "k_stdio.h"
+
+DS::ofstream& DS::ofstream::operator<<(char c)
+{
+}
+
+DS::ofstream& DS::ofstream::operator<<(const char *str)
+{
+  myprint(str);
+  return *this;
+}
+
+DS::ofstream& DS::ofstream::operator<<(int num)
+{
+  myprint(num);
+  return *this;
+}
+
+namespace DS
+{
+  const char *endl="\r\n";
+
+  ofstream cout;
+}
---
+++ myiostream.h
@@ -0,0 +1,24 @@
+#ifndef MYIOSTREAM_H
+#define MYIOSTREAM_H
+
+namespace DS
+{
+  extern const char *endl;
+
+  class ofstream
+  {
+    public:
+      ofstream& operator<<(char c);
+      ofstream& operator<<(const char *str);
+      ofstream& operator<<(int num);
+    private:
+  };
+
+
+  extern ofstream cout;
+}
+
+
+#endif
+
+
