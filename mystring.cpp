0a3779fc04a4b697846dd2125d397fc602863102
descent <descent0@gmail.com>
2015-12-07 13:39:48 +0800

    modified:   mystring.cpp modified:   mystring.h   add:     string::string()     string::operator=()     bool operator>(const DS::string& lhs, const DS::string& rhs )     bool operator>(const DS::string& lhs, const char* rhs )

--- mystring.cpp
+++ mystring.cpp
@@ -1,18 +1,52 @@
 #include "mystring.h"
+#include "myiostream.h"
+#include "mem.h"
+
+DS::string::string():len_(0), str_(0)
+{
+}
+
 DS::string::string(const char *str)
 {
+  len_ = s_strlen(str);
+  str_ = (char *)mymalloc(len_);
+  if (str_ == 0)
+  {
+    // throw exception
+  }
+  s_strcpy(str_, str);
+
+  cout << "string ctor" << endl;
+}
+
+DS::string::~string()
+{
+  myfree(str_);
+  cout << "string ~ctor" << endl;
 }
 
 DS::string& DS::string::operator=(const DS::string& s)
 {
+  cout << "xxx" << endl;
   return *this;
 }
 
-DS::string& DS::string::operator=(const char *s)
+DS::string& DS::string::operator=(const char *str)
 {
+  myfree(str_);
+
+  len_ = s_strlen(str);
+  str_ = (char *)mymalloc(len_);
+  if (str_ == 0)
+  {
+    // throw exception
+  }
+  s_strcpy(str_, str);
+
   return *this;
 }
 
 char& DS::string::operator[](unsigned int idx)
 {
 }
+
--- mystring.h
+++ mystring.h
@@ -1,19 +1,49 @@
 #ifndef MYSTRING_H
 #define MYSTRING_H
 
+#include "type.h"
+#include "k_string.h"
+
 namespace DS
 {
   class string
   {
     public:
+      explicit string();
       explicit string(const char *str);
-    private:
+      ~string();
       string& operator=(const string& s);
-      string& operator=(const char *s);
+      string& operator=(const char *str);
+      //bool operator<(const char *s);
+      //bool operator>(const char *s);
       char& operator[](unsigned int idx);
-
-
+      const char* c_str() const{return str_;}
+      u32 length() const {return len_;}
+      #if 0
+      bool operator>(char const * rhs )
+      {
+      }
+      #endif
+    private:
+      char *str_;
+      u32 len_;
   };
+
 }
+
+#if 1
+static inline bool operator>(const DS::string& lhs, const DS::string& rhs )
+{
+}
+static inline bool operator>(const DS::string& lhs, const char* rhs )
+{
+  int ret = s_strcmp(lhs.c_str(), rhs);
+
+  if (s_strcmp(lhs.c_str(), rhs) > 0)
+    return true;
+  else
+    return false;
+}
+#endif
 
 #endif
