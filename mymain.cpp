811854da8b1b6d2480c981c15877535bfbb36e36
descent <descent0@gmail.com>
2015-12-16 19:28:31 +0800

    add init function array

--- mymain.cpp
+++ mymain.cpp
@@ -1,4 +1,41 @@
+#include "myiostream.h"
+
+using namespace DS;
+
+
+
+typedef int (*InitFunc)(void);
+
+
+int init_timer(void)
+{
+  return 0;
+}
+
+static InitFunc init[]=
+{
+  init_timer,
+  //ramdisk_driver_init,
+  //romfs_init,
+  //mm_init,
+  0
+};
+
+void load_init_boot(InitFunc *init_func)
+{
+  for (int i = 0 ; init_func[i] ; ++i)
+  {
+    init_func[i]();
+  }
+}
+
+void plat_boot(void)
+{
+  load_init_boot(init);
+}
+
 int mymain()
 {
   char *c = new char;
+  cout << "abc" << endl;
 }
