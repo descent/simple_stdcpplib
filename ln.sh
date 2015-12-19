53771ab851717f250e14a29ca164b5053776055a
descent <descent0@gmail.com>
2015-12-19 16:02:00 +0800

    new file:   ln.sh

---
+++ ln.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+ln -s /home/descent/git/progs/queue/cstring.cpp .
+ln -s /home/descent/git/progs/queue/gdeque.h .
+ln -s /home/descent/git/simple_scheme/cell.cpp .
+ln -s /home/descent/git/simple_scheme/cell.h .
+ln -s /home/descent/git/progs/queue/gdeque.cpp .
+ln -s /home/descent/git/progs/queue/cstring.h .
+ln -s /home/descent/git/simple_scheme/s_eval.h .
+ln -s /home/descent/git/simple_scheme/token_container.cpp .
+ln -s /home/descent/git/simple_scheme/token_container.h .
+ln -s /home/descent/git/simple_scheme/s_eval.cpp .
