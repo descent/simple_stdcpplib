e006996cb5525410f7eeaa99b2977047d6c07937
descent <descent0@gmail.com>
2015-12-19 16:07:23 +0800

    fixed inc path

--- rule.mk
+++ rule.mk
@@ -1,6 +1,6 @@
 CFLAGS=-g
-MYCFLAGS=-fno-common -O0 -g -mcpu=cortex-m3 -mthumb -I../inc
-MYCXXFLAGS = -fno-exceptions -fno-rtti -std=c++11
+MYCFLAGS=-fno-common -O0 -g -mcpu=cortex-m3 -mthumb -I./inc
+MYCXXFLAGS = -fno-exceptions -fno-rtti -std=c++11 -I./inc
 LD_FLAGS=-Wl,-T./stm32.ld -nostartfiles
 CXX=arm-none-eabi-g++
 %.dpp: %.cpp
