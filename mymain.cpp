6d1b922b622dd7eb31cde314efc8b28c949ad6bd
descent <descent0@gmail.com>
2015-12-19 15:40:58 +0800

    new file:   mymain.cpp

---
+++ mymain.cpp
@@ -0,0 +1,16 @@
+#include "myiostream.h"
+#include "s_eval.h"
+#include "gdeque.h"
+#include "cstring.h"
+
+using namespace DS;
+
+int mymain()
+{ 
+  mydeque.init();
+  init_eval();
+  Environment *global_env = get_env(0, "global");
+  create_primitive_procedure(global_env);
+  non_os_repl("simple scheme> ", global_env);
+  return 0;
+}
