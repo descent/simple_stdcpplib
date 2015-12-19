813947c8347631b195c0264086b4f48b96b3f194
descent <descent0@gmail.com>
2015-12-19 15:40:16 +0800

    add -std=c++11

--- rule.mk
+++ rule.mk
@@ -1,6 +1,6 @@
 CFLAGS=-g
 MYCFLAGS=-fno-common -O0 -g -mcpu=cortex-m3 -mthumb -I../inc
-MYCXXFLAGS = -fno-exceptions -fno-rtti
+MYCXXFLAGS = -fno-exceptions -fno-rtti -std=c++11
 LD_FLAGS=-Wl,-T./stm32.ld -nostartfiles
 CXX=arm-none-eabi-g++
 %.dpp: %.cpp
