1decdde0f622554464332951f8a8aff1056ed1e2
descent <descent0@gmail.com>
2015-12-23 22:30:13 +0800

    add -std=c++11

--- rule.mk
+++ rule.mk
@@ -4,5 +4,5 @@ MYCXXFLAGS = -fno-exceptions -fno-rtti -std=c++11 -I./inc
 LD_FLAGS=-Wl,-T./stm32.ld -nostartfiles
 CXX=arm-none-eabi-g++
 %.dpp: %.cpp
-	set -e; rm -f $@ ; $(CXX) -MM $(MYCFLAGS) $< > $@.$$$$ ; sed 's,\($*\)\.o[ :]*,\1.o $@ : ,g' < $@.$$$$ > $@ ; rm -f $@.$$$$
+	set -e; rm -f $@ ; $(CXX) -std=c++11 -MM $(MYCFLAGS) $< > $@.$$$$ ; sed 's,\($*\)\.o[ :]*,\1.o $@ : ,g' < $@.$$$$ > $@ ; rm -f $@.$$$$
 
