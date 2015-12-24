493c16b89c7e3e427970f9ed794f8f1b1ae90469
descent <descent0@gmail.com>
2015-12-24 17:11:18 +0800

    for os test

--- mymap.cpp
+++ mymap.cpp
@@ -1,20 +1,30 @@
 #include "mymap.h"
 
+
 #ifdef TEST
+
+#include <iostream>
+
+using namespace DS;
+
 int main(int argc, char *argv[])
 {
-  DS::vector<int> vec;  
+  DS::map<int,int> mymap = {{100, 7}, {101, 5}};
 
-  vec.push_back(1);
-  vec.push_back(2);
-  vec.push_back(3);
-  vec.push_back(4);
-  vec.push_back(5);
-  for (int i=0 ; i < vec.length() ; ++i)
+  mymap.insert({3,21});
+  mymap.insert({1,2});
+
+  for (auto it = mymap.begin() ; it != mymap.end() ; ++it)
   {
-    cout << vec[i] << endl;
+    cout << it->k_ << endl;
   }
-  cout << vec.max_size() << endl;
+#if 1
+  auto it = mymap.find(3);
+  if (it != 0)
+  {
+    cout << "found 3" << endl;
+  }
+#endif
 
   return 0;
 }
