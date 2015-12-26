7135852084af9e77ff636dd446415a437e7c88f7
descent <descent0@gmail.com>
2015-12-26 22:40:34 +0800

    deleted:    stm32f4_simple_scheme

--- rule.mk
+++
@@ -1,8 +0,0 @@
-CFLAGS=-g
-MYCFLAGS=-fno-common -O0 -g -mcpu=cortex-m3 -mthumb -I./inc
-MYCXXFLAGS = -fno-exceptions -fno-rtti -std=c++11 -I./inc
-LD_FLAGS=-Wl,-T./stm32.ld -nostartfiles
-CXX=arm-none-eabi-g++
-%.dpp: %.cpp
-	set -e; rm -f $@ ; $(CXX) -std=c++11 -MM $(MYCFLAGS) $< > $@.$$$$ ; sed 's,\($*\)\.o[ :]*,\1.o $@ : ,g' < $@.$$$$ > $@ ; rm -f $@.$$$$
-
