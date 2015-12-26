56b1039926d32fd9c205d76049db2b5f5c3dcbef
descent <descent0@gmail.com>
2015-12-26 16:29:48 +0800

    new file:   ln.sh

---
+++ stm32f407/ln.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+ln -sf /home/descent/git/cm3_simple_os/stm32f4xx_rcc.h .
+ln -sf /home/descent/git/cm3_simple_os/core_cmInstr.h .
+ln -sf /home/descent/git/cm3_simple_os/core_cm4.h .
+ln -sf /home/descent/git/cm3_simple_os/core_cm4_simd.h .
+ln -sf /home/descent/git/cm3_simple_os/stm32.h .
+ln -sf /home/descent/git/cm3_simple_os/stm32f10x_conf.h .
+ln -sf /home/descent/git/cm3_simple_os/stm32f4xx_usart.h .
+ln -sf /home/descent/git/cm3_simple_os/stm32f4xx.h .
+ln -sf /home/descent/git/cm3_simple_os/stm32f4xx_gpio.h .
+ln -sf /home/descent/git/cm3_simple_os/stm32f10x.h .
+ln -sf /home/descent/git/cm3_simple_os/core_cmFunc.h .
