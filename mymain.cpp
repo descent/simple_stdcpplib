2364b67e6d85e230281f2897022c9226530823c0
descent <descent0@gmail.com>
2015-12-27 09:55:22 +0800

    test new fail

--- mymain.cpp
+++ mymain.cpp
@@ -4,6 +4,11 @@ using namespace DS;
 
 int mymain()
 {
-  char *c = new char;
+  int i=0;
+  for (i=0 ; i < 100 ; ++i)
+  {
+    char *c = new char;
+    cout << "i: " << i << endl;
+  }
   cout << "abc" << endl;
 }
