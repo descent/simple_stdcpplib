1f89822fd5476780500a1f6f6d1dee80fc0f1e16
descent <descent0@gmail.com>
2016-01-05 13:16:49 +0800

    add more test

--- mymain.cpp
+++ mymain.cpp
@@ -1,4 +1,7 @@
 #include "myiostream.h"
+#include "myvec.h"
+#include "mymap.h"
+#include "mystring.h"
 #include "mem.h"
 
 using namespace DS;
@@ -6,6 +9,35 @@ using namespace DS;
 int mymain()
 {
   int i=0;
+
+{
+  vector<string> vec;
+
+  vec.push_back("abc");
+  vec.push_back("xyz");
+  vec.push_back("123");
+  vec.push_back("abcdefghijk!@#$%^&*()");
+
+  for (i=0 ; i < vec.size() ; ++i)
+    cout << vec[i] << endl;
+  cout << "test vec iterator" << endl;
+  for (auto it=vec.begin() ; it!=vec.end() ; ++it)
+    cout << *it << endl;
+}
+
+{
+  map<string, int> str_map;
+
+  str_map.insert({"zxcv", 5});
+  str_map.insert({"abc", 51});
+  str_map.insert({"abcdefghqq", 151});
+  str_map.insert({"111", 99});
+
+  for (auto it=str_map.begin() ; it!=str_map.end() ; ++it)
+    cout << "(" << it->k_ << "," << it->v_ << ")" << endl;
+
+}
+
   for (i=0 ; i < 100 ; ++i)
   {
     char *c = new char;
