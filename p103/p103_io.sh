e14676ebc6a4131873d60c7683e0d145cd2bb260
descent <descent0@gmail.com>
2016-01-11 13:09:55 +0800

    add test stm32_p103_demos exist check

--- p103/p103_io.sh
+++ p103/p103_io.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-git clone https://github.com/descent/stm32_p103_demos.git 
 P103_PREFIX=stm32_p103_demos
+if [ ! -d "$P103_PREFIX" ]; then
+  git clone https://github.com/descent/stm32_p103_demos.git 
+fi
 ln -sf  $P103_PREFIX/demos/common/stm32_p103.c .
 ln -sf  $P103_PREFIX/libraries/CMSIS/CM3/CoreSupport/core_cm3.c .
 ln -sf  $P103_PREFIX/libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/system_stm32f10x.c .
