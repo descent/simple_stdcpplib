70ae8c54bb1dbd1b991d617154d5ea07497d7de3
descent <descent0@gmail.com>
2016-01-11 11:18:01 +0800

    add git clone stm32_p103_demos.git command

--- p103/p103_io.sh
+++ p103/p103_io.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-P103_PREFIX=~/git/jserv-course/stm32_p103_demos
+git clone https://github.com/descent/stm32_p103_demos.git 
+P103_PREFIX=stm32_p103_demos
 ln -sf  $P103_PREFIX/demos/common/stm32_p103.c .
 ln -sf  $P103_PREFIX/libraries/CMSIS/CM3/CoreSupport/core_cm3.c .
 ln -sf  $P103_PREFIX/libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/system_stm32f10x.c .
