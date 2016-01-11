94b1c2d43e87e51614637c06c4bb5b084d7307b0
descent <descent0@gmail.com>
2016-01-11 13:05:09 +0800

    add git clone stm32f4_prog.git

--- stm32f407/ln.sh
+++ stm32f407/ln.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
-ln -sf /home/descent/git/cm3_simple_os/stm32f4xx_rcc.h .
-ln -sf /home/descent/git/cm3_simple_os/core_cmInstr.h .
-ln -sf /home/descent/git/cm3_simple_os/core_cm4.h .
-ln -sf /home/descent/git/cm3_simple_os/core_cm4_simd.h .
-ln -sf /home/descent/git/cm3_simple_os/stm32.h .
-ln -sf /home/descent/git/cm3_simple_os/stm32f10x_conf.h .
-ln -sf /home/descent/git/cm3_simple_os/stm32f4xx_usart.h .
-ln -sf /home/descent/git/cm3_simple_os/stm32f4xx.h .
-ln -sf /home/descent/git/cm3_simple_os/stm32f4xx_gpio.h .
-ln -sf /home/descent/git/cm3_simple_os/stm32f10x.h .
-ln -sf /home/descent/git/cm3_simple_os/core_cmFunc.h .
+STM32f4_DIR=stm32f4_prog
+if [ ! -d "$STM32f4_DIR" ]; then
+  git clone https://github.com/descent/stm32f4_prog.git 
+fi
+#rm -rf stm32f4_prog
+PREFIX_DIR=stm32f4_prog/inc
+ln -sf $PREFIX_DIR/stm32f4xx_rcc.h .
+ln -sf $PREFIX_DIR/core_cmInstr.h .
+ln -sf $PREFIX_DIR/core_cm4.h .
+ln -sf $PREFIX_DIR/core_cm4_simd.h .
+ln -sf $PREFIX_DIR/stm32f4xx_usart.h .
+ln -sf $PREFIX_DIR/stm32f4xx.h .
+ln -sf $PREFIX_DIR/stm32f4xx_gpio.h .
+ln -sf $PREFIX_DIR/core_cmFunc.h .
+#ln -sf $PREFIX_DIR/stm32.h .
+#ln -sf $PREFIX_DIR/stm32f10x_conf.h .
+#ln -sf $PREFIX_DIR/stm32f10x.h .
