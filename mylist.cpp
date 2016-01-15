3aa539e4033833cdf86dee731e4905f38144e742
descent <descent0@gmail.com>
2016-01-15 11:04:00 +0800

    add test code for main. test new/malloc different

--- mylist.cpp
+++ mylist.cpp
@@ -1,20 +1,60 @@
 #include "mylist.h"
 
-#ifdef TEST
+#ifdef TEST_MAIN
+class Composite1
+{
+  public:
+    Composite1()
+    {
+      printf("Composite1\n");
+    }
+};
+
+class Composite2
+{
+  public:
+    Composite2()
+    {
+      printf("Composite2\n");
+    }
+};
+
+class CompositeObj
+{
+  public:
+    CompositeObj()
+    {
+      printf("CompositeObj\n");
+    }
+  private:
+    Composite1 c1_;
+    Composite2 c2_;
+};
 int main(int argc, char *argv[])
 {
-  DS::vector<int> vec;  
+  CompositeObj *co;
 
-  vec.push_back(1);
-  vec.push_back(2);
-  vec.push_back(3);
-  vec.push_back(4);
-  vec.push_back(5);
-  for (int i=0 ; i < vec.length() ; ++i)
+  // co = new CompositeObj{};
+  // co = (CompositeObj*)malloc(sizeof(CompositeObj));
+
+  DS::list<int> list;  
+
+  list.push_back(1);
+  list.push_back(2);
+  list.push_back(3);
+  list.push_back(4);
+  list.push_back(5);
+  for (auto it=list.begin() ; it != list.end() ; ++it)
+  {
+    cout << *it << endl;
+  }
+  #if 0
+  for (int i=0 ; i < list.length() ; ++i)
   {
     cout << vec[i] << endl;
   }
   cout << vec.max_size() << endl;
+  #endif
 
   return 0;
 }
