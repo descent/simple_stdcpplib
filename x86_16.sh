bcec0f1b602fdf6be4a121084fbcfb895019c5c6
descent <descent0@gmail.com>
2016-01-27 15:46:22 +0800

    fixed for ;

--- x86_16.sh
+++ x86_16.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed  '3s/^/\n#ifdef X86_16\n  __asm__(".code16gcc\\n")\n#endif\n/'  *.h
+sed  '3s/^/\n#ifdef X86_16\n  __asm__(".code16gcc\\n");\n#endif\n/'  *.h
 #sed -i '3s/^/\n#ifdef X86_16\n  __asm__(".code16gcc\\n")\n#endif\n/'  *.h
