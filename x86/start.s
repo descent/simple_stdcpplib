0f397a28e61060e124de2d2be7ca29d87a322f90
descent <descent0@gmail.com>
2016-01-28 11:03:10 +0800

    add top_of_stack to set stack, call ResetISR() not enter_main

--- x86/start.s
+++ x86/start.s
@@ -6,10 +6,16 @@ begin:
   mov %ax,%ds
   mov %ax,%es
   mov %ax, %ss
-  mov $0xff00, %sp
+  mov $top_of_stack, %esp
+  xchg %bx, %bx
 
   movw    $0xb800, %ax
   movw    %ax, %gs
 
-  call enter_main
+#  call enter_main
+  call ResetISR
 
+
+LABEL_STACK:
+  .space 10240, 0
+  .set top_of_stack, (. - LABEL_STACK)
