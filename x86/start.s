bffc95c176904c8f9ca78fff843fc486e260ca25
descent <descent0@gmail.com>
2016-01-28 17:02:36 +0800

    add bios_print_char()

--- x86/start.s
+++ x86/start.s
@@ -13,9 +13,17 @@ begin:
   movw    %ax, %gs
 
 #  call enter_main
-  call ResetISR
+  callw ResetISR
 
+
+.global bios_print_char
+bios_print_char:
+    mov     $0x0e,%ah
+    mov     0x4(%esp), %al    /* Char to print */
+    mov     $0x0f, %bl   /* Front color: white */
+    int     $0x10       /* BIOS int 10h, ah=0xe: Print char */
+    ret
 
 LABEL_STACK:
-  .space 10240, 0
+  .space 40960, 0
   .set top_of_stack, (. - LABEL_STACK)
