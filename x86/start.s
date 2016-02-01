2f2fd9a83dbc16fcd09dcf2daad402a7abf49f30
descent <descent0@gmail.com>
2016-02-01 13:49:49 +0800

    move ss from 0x8000 to %cs. stack set to 10240k.
    because the c++ pointer/reference pass to function will get wrong
      arugment.
    
    stack size is 10240k can run simple scheme in very limit environment.

--- x86/start.s
+++ x86/start.s
@@ -7,10 +7,10 @@ begin:
   mov %ax,%es
 
 
-  mov $0x8000,%ax
+  #mov $0x8000,%ax
   mov %ax, %ss
-  mov $0xfff0, %esp
-  #mov $top_of_stack, %esp
+#  mov $0xfff0, %esp
+  mov $top_of_stack, %esp
   xchg %bx, %bx
 
   movw    $0xb800, %ax
@@ -39,6 +39,6 @@ bios_print_char:
     popl   %eax
     retl
 
-#LABEL_STACK:
-#  .space 40960, 0
-#  .set top_of_stack, (. - LABEL_STACK)
+LABEL_STACK:
+  .space 10240, 0
+  .set top_of_stack, (. - LABEL_STACK)
